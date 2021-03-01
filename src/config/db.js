078dc4e4c81e47e4ff5f521ce5010475052feb07
sossowski <kbrzoska97@gmail.com>
2021-03-01 21:41:10 +0100

    dodanie konfiguracji polaczenia z baza danych

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+console.log(process.env.DB_NAME);
+
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);

df068a00122919f04465ba3219c33c207a791261
sossowski <kbrzoska97@gmail.com>
2021-03-01 21:40:30 +0100

    inicjalizacja projektu

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+const db = require('./src/config/db');
+const userRoutes = require('./src/routes/user');
+const postRoutes = require('./src/routes/post');
+const likeRoutes = require('./src/routes/like');
+const initRoutes = require('./src/routes/init');
+
+// db.authenticate()
+//   .then(console.log('connected'))
+//   .catch((error) => console.log(error));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/init', initRoutes);
+app.use('/user', userRoutes);
+app.use('/post', postRoutes);
+app.use('/like', likeRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;

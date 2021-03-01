ea0989e1e6e07068cb2b5ef5bd9659b56485e850
sossowski <kbrzoska97@gmail.com>
2021-03-01 21:41:27 +0100

    zdefiniowanie modeli

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const Like = require('./Like');
+
+const Post = db.define('post', {
+  uid: {
+    type: Sequelize.DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.DataTypes.TEXT,
+    allowNull: false,
+  },
+  localization: {
+    type: Sequelize.DataTypes.GEOMETRY('POINT'),
+    allowNull: false,
+  },
+});
+
+Post.hasMany(Like);
+Like.belongsTo(Post);
+
+module.exports = Post;
---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const Post = require('./Post');
+const Like = require('./Like');
+
+const User = db.define('user', {
+  uid: {
+    type: Sequelize.DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  username: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  isSuperAdmin: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  isAdmin: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+module.exports = User;

008050879e3366de11ff2803dfd772c6faa60fe9
sossowski <kbrzoska97@gmail.com>
2021-03-04 17:44:10 +0100

    refaktor endpointow usera

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,26 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-const User = require('../models/User');
 const { register, login } = require('../controllers/users');
-const { auth } = require('../services/auth');
-
-router.get('/', auth, async (req, res, next) => {
-  try {
-    const users = await User.findAll({});
-
-    console.log(users);
-    res.status(200).json({
-      users,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 router.get('/signup', async (req, res, next) => {
   try {
-    const result = await register(req.body);
+    await register(req.body);
 
     res.status(201).json({ message: 'User created' });
   } catch (error) {

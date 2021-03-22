7a1015d8be70ff902347e0b3e4b42bec8d67440d
sossowski <kbrzoska97@gmail.com>
2021-03-22 18:40:08 +0100

    dodawanie likow do komentarzy

--- src/controllers/like.js
+++ src/controllers/like.js
@@ -1,3 +1,4 @@
+const CustomError = require('../helpers/error');
 const Like = require('../models/Like');
 
 module.exports.setLike = async (postUid, isUpVote, userUid) => {
@@ -12,13 +13,26 @@ module.exports.setLike = async (postUid, isUpVote, userUid) => {
   );
 };
 
-module.exports.addLike = async (postUid, isUpVote, userUid) =>
-  Like.create(
-    { isUpVote, postUid, userUid },
+module.exports.addLike = async (postUid, isUpVote, userUid) => {
+  const result = await Like.findOne({ where: { postUid, userUid } });
+  if (result) throw new CustomError(400, 'Bad Request');
+  return Like.create({ isUpVote, postUid, userUid });
+};
+
+module.exports.setCommentLike = async (commentUid, isUpVote, userUid) => {
+  await Like.update(
+    { isUpVote },
     {
       where: {
         userUid,
-        postUid,
+        commentUid,
       },
     }
   );
+};
+
+module.exports.addCommentLike = async (commentUid, isUpVote, userUid) => {
+  const result = await Like.findOne({ where: { commentUid, userUid } });
+  if (result) throw new CustomError(400, 'Bad Request');
+  return Like.create({ isUpVote, commentUid, userUid });
+};
--- src/routes/like.js
+++ src/routes/like.js
@@ -1,21 +1,26 @@
 const express = require('express');
 
 const router = express.Router();
-const { setLike, addLike } = require('../controllers/like');
+const {
+  setLike,
+  addLike,
+  setCommentLike,
+  addCommentLike,
+} = require('../controllers/like');
 const { auth } = require('../services/auth');
 
-router.patch('/set/post', auth, async (req, res, next) => {
+router.patch('/post', auth, async (req, res, next) => {
   try {
     await setLike(req.body.postUid, req.body.isUpVote, req.data.uid);
     res.status(200).json({
       message: 'success',
     });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-router.post('/add/post', auth, async (req, res, next) => {
+router.post('/post', auth, async (req, res, next) => {
   try {
     const like = await addLike(
       req.body.postUid,
@@ -24,7 +29,31 @@ router.post('/add/post', auth, async (req, res, next) => {
     );
     res.status(201).json(like);
   } catch (error) {
-    console.log(error);
+    next(error);
+  }
+});
+
+router.patch('/comment', auth, async (req, res, next) => {
+  try {
+    await setCommentLike(req.body.commentUid, req.body.isUpVote, req.data.uid);
+    res.status(200).json({
+      message: 'success',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/comment', auth, async (req, res, next) => {
+  try {
+    const like = await addCommentLike(
+      req.body.commentUid,
+      req.body.isUpVote,
+      req.data.uid
+    );
+    res.status(201).json(like);
+  } catch (error) {
+    next(error);
   }
 });
 

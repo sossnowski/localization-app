281491c999de0218c8ef2d35ae5da413982cc4e4
sossowski <kbrzoska97@gmail.com>
2021-04-13 17:32:26 +0200

    obsluga zapisu zdjecia

--- src/services/post.js
+++ src/services/post.js
@@ -1,3 +1,5 @@
+const multer = require('multer');
+const mime = require('mime');
 const Post = require('../models/Post');
 
 module.exports.isUserPostOwner = async (postUid, userUid) => {
@@ -15,3 +17,25 @@ module.exports.postExists = async (uid) => {
 
   return !!post;
 };
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    console.log(req.files);
+    cb(null, `./pictures/${file.fieldname}/`);
+  },
+  filename(req, file, cb) {
+    const uniqueSuffix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
+    cb(
+      null,
+      `${file.fieldname}_${uniqueSuffix}.${mime.getExtension(file.mimetype)}`
+    );
+  },
+});
+
+const upload = multer({ storage });
+const cpUpload = upload.fields([
+  { name: 'profile', maxCount: 1 },
+  { name: 'post', maxCount: 1 },
+]);
+
+module.exports.fileUploader = cpUpload;

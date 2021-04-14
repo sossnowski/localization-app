fd203818330a6ef356e402283de6e2007901324b
sossowski <kbrzoska97@gmail.com>
2021-04-14 20:06:49 +0200

    implemnentacja socketa

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const http = require('http');
+const socketIO = require('socket.io');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+const io = socketIO(server);
+// console.log(io);
+io.on('connection', (socket) => {
+  console.log('Made socket connection');
+  if (socket.handshake.query.auth) {
+    console.log(`user joined.. ${socket.handshake.query.auth}`);
+    socket.join(socket.handshake.query.auth);
+  }
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+app.set('io', io);
 
 server.listen(port);
 console.log(port);

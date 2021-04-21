c8902774a63d3c5982ee179823f24ac71948259f
sossowski <kbrzoska97@gmail.com>
2021-04-21 16:20:42 +0200

    dodanie indexow na date utworzenia

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -1,24 +1,30 @@
 const Sequelize = require('sequelize');
 const db = require('../config/db');
 
-const Notification = db.define('notification', {
-  uid: {
-    type: Sequelize.DataTypes.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    primaryKey: true,
+const Notification = db.define(
+  'notification',
+  {
+    uid: {
+      type: Sequelize.DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    text: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    isUpVote: {
+      type: Sequelize.DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    username: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  text: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false,
-  },
-  isUpVote: {
-    type: Sequelize.DataTypes.BOOLEAN,
-    allowNull: true,
-  },
-  username: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    indexes: [{ fields: ['createdAt'] }],
+  }
+);
 
 module.exports = Notification;
--- src/models/Post.js
+++ src/models/Post.js
@@ -5,21 +5,27 @@ const Comment = require('./Comment');
 const Category = require('./Category');
 const Photo = require('./Photo');
 
-const Post = db.define('post', {
-  uid: {
-    type: Sequelize.DataTypes.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    primaryKey: true,
+const Post = db.define(
+  'post',
+  {
+    uid: {
+      type: Sequelize.DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  title: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+  {
+    indexes: [{ fields: ['createdAt'] }],
+  }
+);
 
 Post.hasMany(Like, {
   onDelete: 'cascade',

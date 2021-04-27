b2f778b7e9a7b64fb60fb229f2d23ca76b6024ce
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2021-04-27 15:18:27 +0200

    pobieranie lokalizacji z wybranego obszatu

--- src/controllers/localization.js
+++ src/controllers/localization.js
@@ -1,7 +1,29 @@
 const Localization = require('../models/Localization');
+const sequelize = require('../config/db');
 
 module.exports.getAll = async () => Localization.findAll({});
 
 module.exports.get = async (uid) => Localization.findOne({ where: { uid } });
 
 module.exports.add = async (data) => Localization.create(data);
+
+module.exports.getFromArea = async (points) => {
+  console.log(points);
+  const { a, b, c, d, e } = points;
+  const contains = sequelize.fn(
+    'ST_CONTAINS',
+    sequelize.fn(
+      'ST_POLYFROMTEXT',
+      `POLYGON((${a.split(',')[0]} ${a.split(',')[1]},${b.split(',')[0]} ${
+        b.split(',')[1]
+      },${c.split(',')[0]} ${c.split(',')[1]},${d.split(',')[0]} ${
+        d.split(',')[1]
+      },${e.split(',')[0]} ${e.split(',')[1]}))`
+    ),
+    sequelize.col('geometry')
+  );
+
+  return Localization.findAll({
+    where: contains,
+  });
+};
--- src/routes/localization.js
+++ src/routes/localization.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { getAll, add, get } = require('../controllers/localization');
+const {
+  getAll,
+  add,
+  get,
+  getFromArea,
+} = require('../controllers/localization');
+const { auth } = require('../services/auth');
 
-router.get('/', async (req, res, next) => {
+router.get('/', auth, async (req, res, next) => {
   try {
     const localizations = await getAll();
 
@@ -13,7 +19,7 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/:uid', async (req, res, next) => {
+router.get('/:uid', auth, async (req, res, next) => {
   try {
     const localization = await get(req.params.uid);
 
@@ -23,7 +29,17 @@ router.get('/:uid', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.get('/:a/:b/:c/:d/:e', auth, async (req, res, next) => {
+  try {
+    const localization = await getFromArea(req.params);
+
+    res.status(200).json(localization);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', auth, async (req, res, next) => {
   try {
     const localization = await add(req.body);
 

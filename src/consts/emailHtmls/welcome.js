89010500d008c334d58eaca7f79af220ae02900f
sossowski <kbrzoska97@gmail.com>
2021-08-26 22:32:01 +0200

    fix

--- src/consts/emailHtmls/welcome.js
+++ src/consts/emailHtmls/welcome.js
@@ -1,6 +1,6 @@
 const { generateConfirmationToken } = require('../../services/auth');
 
-module.exports = (userData) =>
-  `<div style="text-center"><p>Dziękujemy za rejestrację! Potwierdź konto klikając w link! <a href="${
-    process.env.CONFIRMATION_URL
-  }/${generateConfirmationToken(userData)}">Klik</a></p></div>`;
+module.exports = (userData) => {
+  const token = generateConfirmationToken(userData);
+  return `<div style="text-center"><p>Dziękujemy za rejestrację! Potwierdź konto klikając w link! <a href="${process.env.CONFIRMATION_URL}/${token}">Klik</a> Link: ${token}</p></div>`;
+};

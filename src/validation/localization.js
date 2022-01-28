ce4b1f6f29f2933d3e0aaa090aeea92febb68de9
sossnowski <32830266+sossnowski@users.noreply.github.com>
2022-01-28 13:59:28 +0100

    Update localization.js

--- src/validation/localization.js
+++ src/validation/localization.js
@@ -1,7 +1,7 @@
 const { check } = require('express-validator');
 
 module.exports = [
-  check('name').isString().notEmpty().exists(),
+  check('name').isString(),
   check('city').isString().notEmpty().exists(),
   check('geometry.coordinates').isArray().notEmpty().exists(),
   check('geometry.type').isString().notEmpty().exists(),

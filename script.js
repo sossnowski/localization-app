64b14a65a0faae052a52dd97c77175ced815662f
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2022-03-25 06:33:37 +0100

    script improve

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Notification = require('./src/models/Notification');
 const Notification2 = require('./src/models/Notification2');
+const { getPostByComment } = require('./src/controllers/comment');
 
 module.exports.migrateData = async () => {
   await Notification2.destroy({});
@@ -7,19 +8,40 @@ module.exports.migrateData = async () => {
     group: ['text'],
     raw: true,
   });
-  console.log(groupedNotifications);
   for (const notify of groupedNotifications) {
-    const all = await Notification.findAll({
-      where: { text: notify.text },
-      raw: true,
-    });
-    await Notification2.create({
-      text: notify.text,
-      number: all.length,
-      username: all[all.length - 1].username,
-      userUid: notify.targetUid,
-      new: true,
-    });
+    if (notify.text.includes('addComment')) {
+      const comment = await getPostByComment(notify.text.split(':')[1]);
+      console.log(comment.postUid);
+      const notification = await Notification2.findOne({
+        where: {
+          text: `addComment:${comment.postUid}`,
+        },
+      });
+      if (notification) {
+        notification.number += 1;
+        notification.new = true;
+        notification.username = notify.username;
+        notification.save();
+      } else {
+        await Notification2.create({
+          userUid: notify.targetUid,
+          text: `addComment:${comment.postUid}`,
+          username: notify.username,
+        });
+      }
+    } else {
+      const all = await Notification.findAll({
+        where: { text: notify.text },
+        raw: true,
+      });
+      await Notification2.create({
+        text: notify.text,
+        number: all.length,
+        username: all[all.length - 1].username,
+        userUid: notify.targetUid,
+        new: true,
+      });
+    }
     // const uidsToRemove = all.map((a) => a.uid);
     // await Notification.destroy({ where: { uid: uidsToRemove } });
   }

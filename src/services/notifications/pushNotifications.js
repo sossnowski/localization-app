4ec53aa38e425b1c9bddec87d3e1725e835257de
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2022-03-26 14:14:02 +0100

    push notifications text change

--- src/services/notifications/pushNotifications.js
+++ src/services/notifications/pushNotifications.js
@@ -2,38 +2,61 @@ const admin = require('firebase-admin');
 
 const serviceAccount = require('./spotfinderFcm.json');
 
+const subtitles = {
+  user_: 'User',
+  and_: 'and',
+  other_: 'other',
+  others_: 'others',
+  reactComment_: 'reacted to your comment',
+  reactCommentPlural_: 'reacted to your comment',
+  reactPost_: 'reacted to your post',
+  reactPostPlural_: 'reacted to your post',
+  commented_: 'commented your post',
+  commentedPlural_: 'commented your post',
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const messaging = admin.messaging();
 
+const isThereMoreUsersReacted = (number) => {
+  if (number === 1) return '';
+  if (number === 2)
+    return `${subtitles.and_} ${number - 1} ${subtitles.other_}`;
+  return `${subtitles.and_} ${number - 1} ${subtitles.others_}`;
+};
+
 const generateNotificationText = (notification) => {
   const type = notification.text.split(':')[0];
-  const subtitles = {
-    user_: 'Użytkownik',
-    likeComment_: 'lubi twój komentarz',
-    notLikeComment_: 'nie lubi twojego komentarza',
-    likePost_: 'lubi twój post',
-    notLikePost_: 'nie lubi twojego postu',
-    commented_: 'skomentował twój post',
-    showMore_: 'Pokaż więcej',
-    noItems_: 'Nie masz powiadomień',
-  };
+
   switch (type) {
     case 'commentUid':
-      return `${subtitles.user_} ${notification.username} ${
-        notification.isUpVote
-          ? subtitles.likeComment_
-          : subtitles.notLikeComment_
+      return `${subtitles.user_} ${
+        notification.username
+      } ${isThereMoreUsersReacted(notification.number)} ${
+        notification.number > 1
+          ? subtitles.reactCommentPlural_
+          : subtitles.reactComment_
       }`;
     case 'postUid':
-      return `${subtitles.user_} ${notification.username} ${
-        notification.isUpVote ? subtitles.likePost_ : subtitles.notLikePost_
+      return `${subtitles.user_} ${
+        notification.username
+      } ${isThereMoreUsersReacted(notification.number)} ${
+        notification.number > 1
+          ? subtitles.reactPostPlural_
+          : subtitles.reactPost_
       }`;
 
     case 'addComment':
-      return `${subtitles.user_} ${notification.username} ${subtitles.commented_} `;
+      return `${subtitles.user_} ${
+        notification.username
+      } ${isThereMoreUsersReacted(notification.number)} ${
+        notification.number > 1
+          ? subtitles.commentedPlural_
+          : subtitles.commented_
+      } `;
     default:
       return 'nieznany';
   }

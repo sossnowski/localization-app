7f917af47ba33363676b5e43b2bd39457c861ba1
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2022-03-26 11:03:45 +0100

    fix

--- src/routes/init.js
+++ src/routes/init.js
@@ -4,16 +4,12 @@ const { forceInit } = require('../controllers/init');
 const router = express.Router();
 const db = require('../config/db');
 const { seedCategories } = require('../config/categorySeed');
-const { migrateData, assignLikes } = require('../../script');
 // const Notification = require('../models/Notification');
 
 router.get('/', async (req, res, next) => {
   try {
     await db.sync({ alter: true });
     // await seedCategories();
-    // await Notification.sync();
-    await migrateData();
-    await assignLikes();
 
     res.status(200).json({
       success: true,
